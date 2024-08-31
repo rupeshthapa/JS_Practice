f3ca96e4b7ab39fd8b983b49cfcb311ec3d24d13
Rupesh Thapa <thaparupesh767@gmail.com>
2024-08-31 12:13:14 +0545

    primitive datatypes practice

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = "Hello";
+let b = 10;
+let c = null;
+let d;
+let e = true;
+let f = Symbol(a);
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
