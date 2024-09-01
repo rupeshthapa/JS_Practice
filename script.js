b11b3469426e54a5f0c63bcd4b1fcf80c27d2290
Rupesh Thapa <thaparupesh767@gmail.com>
2024-09-01 21:48:02 +0545

    Operators

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-let a = "Hello";
-let b = 10;
-let c = null;
-let d;
-let e = true;
-let f = Symbol(a);
+// Day -1
+// let a = "Hello";
+// let b = 10;
+// let c = null;
+// let d;
+// let e = true;
+// let f = Symbol(a);
 
-console.log(typeof(a));
-console.log(typeof(b));
-console.log(typeof(c));
-console.log(typeof(d));
-console.log(typeof(e));
-console.log(typeof(f));
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(e));
+// console.log(typeof(f));
+
+//Day -2
+let a= 10
+let b = 20
+//Arithmetic Operators
+console.log(a+b);
+console.log(b-a);
+console.log(b/a);
+console.log(b%a);
+console.log(a*b);
+
+//Comparison Operators
+console.log(a==b);
+console.log(a===b);
+console.log(a<=b);
+console.log(a>=b);
+console.log(a!=b);
+console.log(a!==b);
+console.log(a>b);
+console.log(a<b);
+
+//Logical Operators
+let p = true;
+let q = false;
+
+console.log(p&&q);
+console.log(p||q);
+console.log(!p);
+console.log(!q);
+
