e3371e90221ad71bfeeb22f3b09f4ea98f436fcc
Rupesh Thapa <thaparupesh767@gmail.com>
2024-12-12 12:53:58 +0545

    basic learning

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-console.log("JavaScript is connected!");
+let birthYear = parseInt(prompt("Enter your birthyear"));
+let currentYear = new Date().getFullYear();
 
-document.getElementById("")
+if(isNaN(birthYear) || birthYear <= 0){
+    console.log("Invalid..")
+}else{
+let age = currentYear - birthYear;
+console.log("Your age is: " + age);
+
+
+
+if(age % 3 === 0 && age % 5 === 0){
+    console.log("Can be divided by both 3 and 5.");
+}else{
+    console.log("Cannot be divided by 3 and 5.");
+}
+
+
+let num = parseInt(prompt("Enter a number:"));
+if(isNaN(num)){
+    console.log("Invalid");
+}else{
+let squareOfNum = num * num;
+
+console.log("The square of number is: " + squareOfNum);
+}
+}
